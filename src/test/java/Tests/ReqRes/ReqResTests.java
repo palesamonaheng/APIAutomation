952b080c484d453aaa4d576a529154143824b3ba
nkosi <nkosi.cele@simplify.africa>
2024-06-12 19:36:34 +0200

    completed the create employye test

--- src/test/java/Tests/ReqRes/ReqResTests.java
+++ src/test/java/Tests/ReqRes/ReqResTests.java
@@ -1,4 +1,24 @@
 package Tests.ReqRes;
 
+import io.qameta.allure.*;
+import org.testng.annotations.Test;
+import static Common.CoomonTestData.Create_Success_Status_Code;
+import static Common.RequestBuilder.createEmployeeResponse;
+import static org.hamcrest.Matchers.containsStringIgnoringCase;
+
+
+@Test
+@Feature("ReqRes")
+@Story("Create new employee")
 public class ReqResTests {
+
+    @Description("As an api user i want to create a new employee")
+    @Severity(SeverityLevel.CRITICAL)
+    public void createEmployeeTests() {
+        createEmployeeResponse().
+                then().
+                assertThat().
+                statusCode(Create_Success_Status_Code).
+                body("name", containsStringIgnoringCase("Nkosi"));
+    }
 }
